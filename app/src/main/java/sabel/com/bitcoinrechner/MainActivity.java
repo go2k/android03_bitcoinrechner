4a145d114644d39ca6ad33639ba0513ac4b0edd3
Michael König <m.koenig@go2k.de>
2017-11-23 10:33:50 +0100

    init

---
+++ app/src/main/java/sabel/com/bitcoinrechner/MainActivity.java
@@ -0,0 +1,40 @@
+package sabel.com.bitcoinrechner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etEuro;
+    private EditText etBitcoin;
+    private Button btnBerechenen;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etEuro = findViewById(R.id.et_euro);
+        etBitcoin = findViewById(R.id.et_bitcoin);
+        btnBerechenen = findViewById(R.id.btn_umrechnen);
+
+        btnBerechenen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Double.parseDouble(etEuro.toString()) > 0) {
+
+
+                }
+            }
+        });
+
+
+
+    }
+
+
+}
